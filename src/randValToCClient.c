dc9413a3ff531d48c9afe3d0d762fba1cf9f6052
Axel Vanherle <axelvanherle@outlook.com>
2022-12-29 11:39:26 +0100

    Added sending for x amount of s at x frequency

--- src/randValToCClient.c
+++ src/randValToCClient.c
@@ -4,9 +4,9 @@
 #include <time.h>
 
 #ifdef _WIN32
-#include <winsock2.h>	//for all socket programming
-#include <ws2tcpip.h>	//for getaddrinfo, inet_pton, inet_ntop
-#include <unistd.h>		//for close
+#include <winsock2.h> //for all socket programming
+#include <ws2tcpip.h> //for getaddrinfo, inet_pton, inet_ntop
+#include <unistd.h>	  //for close
 #else
 #include <sys/socket.h> //for sockaddr, socket, socket
 #include <sys/types.h>	//for size_t
@@ -27,46 +27,64 @@ int main()
 	printf("Enter Port: ");
 	scanf("%s", Port);
 
-	#ifdef _WIN32
-	WSADATA wsaData;
-	WSAStartup( MAKEWORD(2,0), &wsaData );
-	#else
-	// Empty for linux.
-	#endif
+	// This is used to we can set how long we want the program to run.
+	// Enter time in seconds.
+	int timeToRun = 5*60;
+	// Frequency at which we want to send a packet in seconds.
+	int packetFrequency = 30;
 
 	// Seed the rand function
 	srand(time(0));
 
-	// Set up the struct
-	struct addrinfo internet_address_setup;
-	struct addrinfo *internet_address = NULL;
-	memset(&internet_address_setup, 0, sizeof internet_address_setup);
+	// Add a timer to the program.
+	clock_t timer_start, timer_end;
+    timer_start = clock();
 
-	// We want IPV4 and UDP.
-	internet_address_setup.ai_family = AF_INET;
-	internet_address_setup.ai_socktype = SOCK_DGRAM;
-	getaddrinfo(IP, Port, &internet_address_setup, &internet_address);
+	while (timeToRun >= packetFrequency)
+	{
+		#ifdef _WIN32
+				WSADATA wsaData;
+				WSAStartup(MAKEWORD(2, 0), &wsaData);
+		#else
+				// Empty for linux.
+		#endif
 
-	// Get me a damn socket! Grrrrrr!
-	int internet_socket = socket(internet_address->ai_family, internet_address->ai_socktype, internet_address->ai_protocol);
+		// Set up the struct
+		struct addrinfo internet_address_setup;
+		struct addrinfo *internet_address = NULL;
+		memset(&internet_address_setup, 0, sizeof internet_address_setup);
 
-	// Prepare the random value to be sent over UDP.
-	char str[4];
-	sprintf(str, "%d", (rand() % 100) + 1);
+		// We want IPV4 and UDP.
+		internet_address_setup.ai_family = AF_INET;
+		internet_address_setup.ai_socktype = SOCK_DGRAM;
+		getaddrinfo(IP, Port, &internet_address_setup, &internet_address);
 
-	// Send my packet!
-	sendto(internet_socket, str, strlen(str), 0, internet_address->ai_addr, internet_address->ai_addrlen);
+		// Get me a damn socket! Grrrrrr!
+		int internet_socket = socket(internet_address->ai_family, internet_address->ai_socktype, internet_address->ai_protocol);
 
-	// Clean up
-	freeaddrinfo(internet_address);
-	close(internet_socket);
+		// Prepare the random value to be sent over UDP.
+		char str[4];
+		sprintf(str, "%d", (rand() % 100) + 1);
 
-	// Uncomment if you're working on a windows machine to test.
-	#ifdef _WIN32
-	WSACleanup();
-	#else
-	// Empty for linux.
-	#endif
-	
+		// Send my packet!
+		sendto(internet_socket, str, strlen(str), 0, internet_address->ai_addr, internet_address->ai_addrlen);
+
+		// Clean up
+		freeaddrinfo(internet_address);
+		close(internet_socket);
+
+		#ifdef _WIN32
+				WSACleanup();
+		#else
+		// Empty for linux.
+		#endif
+
+		timeToRun -= packetFrequency;
+		sleep(packetFrequency);
+	}
+
+	timer_end = clock(); // End the timer.
+    double time_spent = (double)(timer_end - timer_start) / CLOCKS_PER_SEC;
+    printf("\nProgram stopped, it ran for %.3fs.\n", time_spent);
 	return 0;
 }
