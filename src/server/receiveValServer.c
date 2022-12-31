245ca4d698fdbfe36544bdb3ac469193c211e071
Axel Vanherle <94362354+axelvanherle@users.noreply.github.com>
2022-12-31 15:26:31 +0100

    Update receiveValServer.c

--- src/server/receiveValServer.c
+++ src/server/receiveValServer.c
@@ -105,7 +105,7 @@ void *receiveThread()
 
 int main(int argc, char *argv[])
 {
-	printf("Welcome to receiveValServer.");
+	printf("Welcome to receiveValServer.\n");
 	// Initialization
 	#ifdef _WIN32
 		OSInit();
